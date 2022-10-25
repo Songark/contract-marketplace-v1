0a57aee77b4df979a8c686ea1fb2d6817a4870d1
BestPartnerForYou <arksong123@gmail.com>
2022-10-25 12:49:19 -0400

    Marketplace besides ethereum payment

--- scripts/constants.js
+++ scripts/constants.js
@@ -13,6 +13,7 @@ const nftSellers = [
 
 const treasury = "0xF0d096D33559cDc5f527435b82073c108D6c3107";
 const gameWallet = "0xd5439F21fd46f2eA74563443626f0246E268983f";
+const gamePlayV2 = "0x8004422baEb59146d548fb0C238848CCe4B1B31F";
 const TokenTypes_membershipNFT = 0;
 const TokenTypes_customNFT = 1;
 const TokenTypes_erc20Token = 2;
